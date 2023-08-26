2e5b31d1a3774383fec79b845ffd8379c29137d4
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2023-08-26 23:47:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    textColor:{
+      'primary':'#B01C87',
+      
+    },
+    
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
+
